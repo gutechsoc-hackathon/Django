3016b2e01f50f997adb5128ef3ddb8897a1bb6f6
Gary Blackwood <gareeblackwood@gmail.com>
2013-11-02 17:31:53 +0000

    Improved some UI.

--- GUTSHackathon/urls.py
+++ GUTSHackathon/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 urlpatterns = patterns('',
-                       url(r'^$', 'users.views.register'),
+                       url(r'^$', 'django.contrib.auth.views.login', {'template_name':'users/login.html', 'extra_context':{'next':'/user/home'}}, name='login'),
                        url(r'^user/', include('users.urls', namespace='user')),
-    )
+)
 
 # Service MEDIA from here if in DEBUG mode.
 if settings.DEBUG:
