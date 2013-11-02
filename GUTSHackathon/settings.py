c5a7c320d503feeebbcb65e6254f4cd6ee83b7d1
Gary Blackwood <gareeblackwood@gmail.com>
2013-11-02 12:03:40 +0000

    Added base.html template.

--- GUTSHackathon/settings.py
+++ GUTSHackathon/settings.py
@@ -17,12 +17,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'database.db'
     }
 }
 
