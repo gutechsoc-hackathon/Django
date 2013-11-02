ab00ad762b66e548d3cdee5980f0d4bac2317ad1
Gary Blackwood <gareeblackwood@gmail.com>
2013-11-02 16:15:22 +0000

    Implemented user association with app tracker devices.

--- GUTSHackathon/urls.py
+++ GUTSHackathon/urls.py
@@ -4,12 +4,11 @@ from django.conf import settings
 urlpatterns = patterns('',
                        url(r'^$', 'users.views.register'),
                        url(r'^user/', include('users.urls', namespace='user')),
-)
+    )
 
 # Service MEDIA from here if in DEBUG mode.
 if settings.DEBUG:
     urlpatterns += patterns(
         'django.views.static',
         (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}))
-    
     
