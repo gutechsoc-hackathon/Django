6f251dd57343fdde8129784abd6e8ec351ec2482
Gary Blackwood <gareeblackwood@gmail.com>
2013-11-03 15:21:49 +0000

    Added mobile number to registration.

--- users/forms.py
+++ users/forms.py
@@ -12,11 +12,16 @@ class RegisterForm(forms.Form):
                                                               'placeholder':'Last name'
                                                               }))
     
+    mobile_number = forms.CharField(label="", required=True, max_length=30,
+                                widget=forms.TextInput(attrs={'class':'form-control',
+                                                              'placeholder':'Mobile number'
+                                                              }))
+
     email = forms.EmailField(label="", required=True,
 widget=forms.TextInput(attrs={'class':'form-control',
                                                            'placeholder':'Email address'
                                                            }))
-
+    
     password = forms.CharField(label="", required=True, min_length=8,
                                widget=forms.PasswordInput(attrs={'class':'form-control',
                                                                  'placeholder':'Password'
